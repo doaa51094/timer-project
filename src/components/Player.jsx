3746aaa53346018f16ed346b541c54d2249e2f24
doaa51094 <112436865+doaa51094@users.noreply.github.com>
2024-07-24 16:25:42 +0300

    create card fo timer challenge

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,16 +1,18 @@
 import { useRef, useState } from "react";
 
 export default function Player() {
-  const [enterdPlayerName,setEnterdPlayerName]=useState(null);
-  const playerName=useRef();
- 
+  const [enterdPlayerName, setEnterdPlayerName] = useState(null);
+  const playerName = useRef();
+
   function handleClick() {
-    setEnterdPlayerName(playerName.current.value)  }
+    setEnterdPlayerName(playerName.current.value);
+    playerName.current.value = "";
+  }
   return (
     <section id="player">
-      <h2>Welcome {enterdPlayerName ?? 'unknown entity'}</h2>
+      <h2>Welcome {enterdPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input ref={playerName} type="text"/>
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
